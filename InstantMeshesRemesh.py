070c83606aa437e26edfd4d3d99d804c39495ebb
Thomas <tvmann.3d@gmail.com>
2018-06-15 10:56:29 +0200

    batch function and open in UI

--- InstantMeshesRemesh.py
+++ InstantMeshesRemesh.py
@@ -9,6 +9,7 @@ import bpy
 import os
 import subprocess
 import tempfile
+import shutil
 
 
 class InstantMeshesRemeshPrefs(bpy.types.AddonPreferences):
@@ -26,6 +27,27 @@ class InstantMeshesRemeshPrefs(bpy.types.AddonPreferences):
         layout.prop(self, "filepath")
 
 
+class InstantMeshesRemeshBatch(bpy.types.Operator):
+    """Remesh selected objects with last used settings"""
+    bl_idname = "object.instant_meshes_remesh_batch"
+    bl_label = "Instant Meshes Remesh BATCH"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        s = bpy.context.selected_objects
+        for other_obj in bpy.data.objects:
+                    other_obj.select = False
+        for i in s:
+            i.select = True
+            bpy.context.scene.objects.active = i
+            bpy.ops.object.instant_meshes_remesh()
+            for slot, mat in enumerate(i.data.materials):
+                bpy.data.objects[i.name+'_remesh'].data.materials[slot] = mat.copy() 
+                bpy.data.objects[i.name+'_remesh'].data.materials[slot].diffuse_color = (0.3,1,0.3)
+            i.select = False
+            bpy.context.scene.objects.active = None
+        return {'FINISHED'}
+
 class InstantMeshesRemesh(bpy.types.Operator):
     """Remesh by using the Instant Meshes program"""
     bl_idname = "object.instant_meshes_remesh"
@@ -40,6 +62,7 @@ class InstantMeshesRemesh(bpy.types.Operator):
     crease = bpy.props.IntProperty(name="Crease Degree", description="Dihedral angle threshold for creases", default=0, min=0, max=100)
     verts = bpy.props.IntProperty(name="Vertex Count", description="Desired vertex count of the output mesh", default=2000, min=200, max=50000)
     smooth = bpy.props.IntProperty(name="Smooth iterations", description="Number of smoothing & ray tracing reprojection steps (default: 2)", default=2, min=0, max=10)
+    openUI = bpy.props.BoolProperty(name="Open in InstantMeshes", description="Opens the selected object in Instant Meshes and imports the result when you are done.", default=False)
     
     loc = None
     rot = None
@@ -97,13 +120,18 @@ class InstantMeshesRemesh(bpy.types.Operator):
             options.append('-b')
             
         cmd = [exe] + options + [orig]
-        subprocess.run(cmd)
+        if self.openUI:
+            os.chdir(os.path.dirname(orig))
+            shutil.copy2(orig, output)
+            subprocess.run([exe, output])
+            self.openUI = False
+        else:
+            subprocess.run(cmd)
         
         bpy.ops.import_scene.obj(filepath=output, 
                                 use_smooth_groups=False,
                                 use_image_search=False)
         imported_mesh = bpy.context.selected_objects[0]
-        #imported_mesh.parent = mesh.parent
         imported_mesh.location = self.loc
         imported_mesh.rotation_euler = self.rot
         imported_mesh.scale = self.scl
@@ -126,23 +154,28 @@ class InstantMeshesRemesh(bpy.types.Operator):
                                         use_auto_transform=False, use_object_transform=True, use_max_distance=False, 
                                         max_distance=1.0, ray_radius=0.0, islands_precision=0.1, layers_select_src='ACTIVE',
                                         layers_select_dst='ACTIVE', mix_mode='REPLACE', mix_factor=1.0)
+        mesh.select = False
         mesh.hide = True
         mesh.hide_render = True
-        
+        imported_mesh.select = False
+
         return {'FINISHED'}
 
 
 def menu_func(self, context):
+    self.layout.operator(InstantMeshesRemeshBatch.bl_idname)
     self.layout.operator(InstantMeshesRemesh.bl_idname)
 
 def register():
     bpy.utils.register_class(InstantMeshesRemesh)
+    bpy.utils.register_class(InstantMeshesRemeshBatch)
     bpy.utils.register_class(InstantMeshesRemeshPrefs)
     bpy.types.VIEW3D_MT_object.append(menu_func)
 
 
 def unregister():
     bpy.utils.unregister_class(InstantMeshesRemesh)
+    bpy.utils.unregister_class(InstantMeshesRemeshBatch)
     bpy.utils.unregister_class(InstantMeshesRemeshPrefs)
     bpy.types.VIEW3D_MT_object.remove(menu_func)
 
